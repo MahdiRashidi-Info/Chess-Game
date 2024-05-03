8a8f86184e7ffa84eff8bc53401fa714bb561cd3
mahdi rashidi <rashidi.mahdi2001@gmail.com>
2024-05-03 20:37:53 +0330

    add color utils

---
+++ Assets/Scripts/ColorUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace ChessGame
+{
+    public class ColorUtils : MonoBehaviour
+    {
+        #region Singleton
+
+        public static ColorUtils instance;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+        #endregion
+
+        [SerializeField] private Color blackPieceColor;
+        [SerializeField] private Color whitePieceColor;
+        [SerializeField] private Color activeColor;
+        [SerializeField] private Color blackBoardColor;
+        [SerializeField] private Color whiteBoardColor;
+
+
+    }
+}
